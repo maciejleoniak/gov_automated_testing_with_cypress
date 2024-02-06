36f10a0e73a9ee8574994bbfcd20699a2f193b3c
7799043 <leoniak.maciek1@gmail.com>
2024-02-06 19:31:31 +0100

    .

--- cypress/components/SearchFieldComponent.js
+++ cypress/components/SearchFieldComponent.js
@@ -1,7 +1,8 @@
 class SearchFieldComponent {
 
     searchFieldSelector = '#query';
-    searchButtonSelector = 'body > main > div:nth-child(1) > section.gov-services.blue-bar-row.blue-bar-row--epolak > div > div > div.blue-bar-content > form > button';
+    searchButtonSelector ='button[type="submit"]';
+    // searchButtonSelector = 'body > main > div:nth-child(1) > section.gov-services.blue-bar-row.blue-bar-row--epolak > div > div > div.blue-bar-content > form > button';
 
     SearchBar(searchQuery) {
         cy.get(this.searchFieldSelector)
